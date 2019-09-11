73da732802811259756a65026a8de95bf97707c2
lavedon <lavedon@gmail.com>
2019-09-11 18:23:53 -0400

    added readme.

---
+++ audit.ps1
@@ -0,0 +1,10 @@
+# Basic Template Not Yet Done
+Remove-Variable html
+$clientImageFile = 'client.jpg';
+
+# Graphics to convert to bits
+$imageClient = $clientImageFile;
+$imageGmail = 'gmail.png';
+$imageOutlook = 'outlook.png';
+
+
---
+++ readme.md
@@ -0,0 +1,3 @@
+<h1>Email Audit Tools</h1>
+<h4>Under Construction</h4>
+<img src="https://giphy.com/gifs/3o72FkiKGMGauydfyg/html5" />
