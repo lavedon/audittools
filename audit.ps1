8f35ff16fc012ecf9bb851f0022699cb2b3d2281
lavedon <lavedon@gmail.com>
2019-09-11 22:18:39 -0400

    pulls a bunch of records.  Trouble displaying txt records

--- audit.ps1
+++ audit.ps1
@@ -1,5 +1,5 @@
 # Basic Template Not Yet Done
-# Remove-Variable html
+Remove-Variable html
 
 # Import Client Information
 # The Name is on the first line
@@ -12,8 +12,6 @@ $imageGmail = "c:\inbox\htmlreports\audit\gmail.png";
 $imageOutlook = "c:\inbox\htmlreports\audit\outlook.png";
 
 
-
-
 # @TODO use an array 
 $clientBits = [Convert]::ToBase64String((Get-Content $imageClient -Encoding Byte))
 $gmailBits = [Convert]::ToBase64String((Get-Content $imageGmail -Encoding Byte))
@@ -23,8 +21,20 @@ $clientImageHTML = "<img src=data:image/png;base64,$($clientBits) alt='db utiliz
 $gmailImageHTML = "<img src=data:image/png;base64,$($gmailBits) alt='db utilization' width='150' height='150'/>";    
 $outlookImageHTML = "<img src=data:image/png;base64,$($outlookBits) alt='db utilization' width='150' height='150'/>";    
 
-$head = @"
+# Get data from domains
+# @TODO Put This into an array
+[xml]$basicDNS = $domains | foreach-object { resolve-dnsname $_ } | ConvertTo-HTML -fragment
+# @TODO other dns-lookups
+# A records
+[xml]$ARecords = $domains | foreach-object { Resolve-DnsName -name $_ -type A } | ConvertTo-Html -fragment 
+# MX Records
+[xml]$MXRecords = $domains | foreach-object { Resolve-DnsName -name $_ -type MX } | ConvertTo-Html -fragment 
+# CNAME Records
+[xml]$CNAMERecords = $domains | foreach-object { Resolve-DnsName -name $_ -type CNAME } | ConvertTo-Html -fragment
+# TXT Records
+[xml]$TXTRecords = $domains | foreach-object { Resolve-DnsName -name $_ -type TXT } | ConvertTo-Html -fragment
 
+$head = @"
 <!DOCTYPE html>
 <html lang="en">
 <meta charset="utf-8"/>
@@ -58,26 +68,36 @@ table {
 .warn {
     background-color: yellow;
 }
-
-
 </style>
-"
+"@
 
 
 $body = @"
-$clientImageHTML
+<a href="https://github.com/lavedon/audittools">Made with Luke's Audit Tools</a>
+<br>Report for $($clientInfo[0]) &nbsp;&nbsp;&nbsp;$clientImageHTML<br /><i>$(Get-Date)</i>
+$($basicDNS.InnerXml)
+<br />
+<h4>A Records</h4>
+$($ARecords.InnerXml)
+<br />
+<h4>MX Records</h4>
+$($MXRecords.InnerXml)
+<br />
+<h4>CNAME Records</h4>
+$($CNAMERecords.InnerXml)
+<br />
+<h4>TXT Records</h4>
+$($TXTRecords.InnerXml)
 <br />
 $gmailImageHTML
 <br />
 $outlookImageHTML
-<a href="https://github.com/lavedon/audittools">Made with Luke's Audit Tools</a>
 "@
 
-$domains = Get-Content .\domains.txt 
+$domains = Get-Content .\domains.txt
+ 
 
+ConvertTo-HTML -head $head -body $body |
+Out-file ".\report.html" -Encoding ascii;
 
-
-ConvertTo-HTML -head $head -PostContent "<br>Report for $($clientInfo[0])<br /><i>$(Get-Date)</i>" -body $body |
-Out-file "$env:temp\report.html" -Encoding ascii;
-
-Invoke-Item "$env:temp\report.html";
+Invoke-Item ".\report.html";
