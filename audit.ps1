7e4995e1a58a65f2b7dded960f6c77dcc6f90d26
lavedon <lavedon@gmail.com>
2019-09-11 20:38:26 -0400

    basic images work.

--- audit.ps1
+++ audit.ps1
@@ -1,10 +1,50 @@
 # Basic Template Not Yet Done
-Remove-Variable html
-$clientImageFile = 'client.jpg';
+# Remove-Variable html
+
+# Import Client Information
+# The Name is on the first line
+# The client logo is on the second
+$clientInfo = Get-Content .\client.txt
 
 # Graphics to convert to bits
-$imageClient = $clientImageFile;
-$imageGmail = 'gmail.png';
-$imageOutlook = 'outlook.png';
+$imageClient = $clientInfo[1];
+$imageGmail = "c:\inbox\htmlreports\audit\gmail.png";
+$imageOutlook = "c:\inbox\htmlreports\audit\outlook.png";
 
 
+
+
+# @TODO use an array 
+$clientBits = [Convert]::ToBase64String((Get-Content $imageClient -Encoding Byte))
+$gmailBits = [Convert]::ToBase64String((Get-Content $imageGmail -Encoding Byte))
+$outlookBits = [Convert]::ToBase64String((Get-Content $imageOutlook -Encoding Byte))
+
+$clientImageHTML = "<img src=data:image/png;base64,$($clientBits) alt='db utilization' width='150' height='150'/>";
+$gmailImageHTML = "<img src=data:image/png;base64,$($gmailBits) alt='db utilization' width='150' height='150'/>";    
+$outlookImageHTML = "<img src=data:image/png;base64,$($outlookBits) alt='db utilization' width='150' height='150'/>";    
+
+$head = @"
+
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="utf-8"/>
+<link rel="stylesheet" type="text/css" href="style.css">
+"
+
+
+$body = @"
+
+<a href="https://github.com/lavedon/audittools">Made with Luke's Audit Tools</a>
+"@
+
+$domains = Get-Content .\domains.txt 
+
+$domains | foreach-object {
+
+    resolve-dnsname -name $_;
+}
+
+ConvertTo-HTML -head $head -PostContent "<br>Report for $($clientInfo[1])<br /><i>$(Get-Date)</i>" -body $body |
+Out-file "$env:temp\report.html" -Encoding ascii;
+
+Invoke-Item "$env:temp\report.html";
