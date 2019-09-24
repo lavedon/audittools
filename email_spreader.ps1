f449aebfef3e29747967b4255da7f0ff54647c2e
lavedon <lavedon@gmail.com>
2019-09-23 22:23:48 -0400

    deciding if I want separate arrays for emails and domains.

--- email_spreader.ps1
+++ email_spreader.ps1
@@ -1,1 +1,14 @@
-﻿
+﻿$emails = @();
+$domains = @();
+
+$csv = import-csv email_test.csv;
+
+foreach($item in $csv) {
+    $domains += $item | select-object -property email
+}
+
+# Try something like this
+###
+# foreach($item in $csv) {
+#     $domains += $item | select-object -property email | $_.Email.split("@")[1];
+#}
