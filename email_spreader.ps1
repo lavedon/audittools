d0405c48af374676e7838bcd0d29c8639ebef705
lavedon <lavedon@gmail.com>
2019-09-25 01:14:34 -0400

    now adding in checking each domain for NS record

--- email_spreader.ps1
+++ email_spreader.ps1
@@ -1,14 +1,31 @@
-﻿$emails = @();
-$domains = @();
+﻿[array]$emails = @();
+[array]$domains = @();
+[array]$NSRecords = @();
 
 $csv = import-csv email_test.csv;
-
 foreach($item in $csv) {
-    $domains += $item | select-object -property email
+    $emails += $item | Select-Object -Property email;
 }
 
-# Try something like this
-###
-# foreach($item in $csv) {
-#     $domains += $item | select-object -property email | $_.Email.split("@")[1];
-#}
+foreach($item in $emails) {
+     $curDomain = $null;
+     $curDomain = $item.Email.split("@")[1];
+     if ($curDomain) {$domains += $curDomain};
+}
+
+Write-Host "Checking NS Records";
+
+foreach($domain in $domains) {
+    
+    $result = $null;
+    $result = Resolve-DnsName -name $domain -type NS
+    Write-Host $result;
+    
+}
+
+Write-Host "Current domains are $domains";
+
+# Example of making an array of NS Records
+[array]$currNSRecord = Resolve-DnsName -name gngrninja.com -type NS;
+$currNSRecord = $currNSRecord | ForEach-Object { $_.Name };
+$currNSRecord | % { if($_ -match "square") {write-host "squarespace"}};
